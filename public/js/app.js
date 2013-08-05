8e89c8c2de24a6105dab588a84740b25091b2948
davidbeijinho <davidbeijinho@gmail.com>
2013-08-05 15:02:49 +0100

    primeiro do pc

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('myApp', ['myApp.filters', 'myApp.directives'])
+.
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index',
+        controller: AppCtrl
+      }).
+      when('/ambiente', {
+        templateUrl: 'partials/ambiente',
+        controller: AmbienteCtrl
+      }).
+      when('/ensaios', {
+        templateUrl: 'partials/ensaios',
+        controller: EnsaiosCtrl
+      }).
+       when('/rega', {
+        templateUrl: 'partials/rega',
+        controller: RegaCtrl
+      }).
+       when('/dashboard', {
+        templateUrl: 'partials/dashboard',
+        controller: DashboardCtrl
+      }).
+       when('/reports', {
+        templateUrl: 'partials/reports',
+        controller: ReportsCtrl
+      });//.
+      // otherwise({
+      //   redirectTo: '/'
+      // });
+    $locationProvider.html5Mode(true);
+  }]);
